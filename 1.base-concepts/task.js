189031565c5502cb791158be04753505da10aa48
NickAte <72125393+NickAte@users.noreply.github.com>
2023-03-01 17:48:28 +0300

    Первая задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = (b**2) - 4*a*c;
+  let x = (-b) / 2*a;
+  let x1 = (-b + Math.sqrt(D) )/(2*a);
+  let x2 = (-b - Math.sqrt(D) )/(2*a);
+
+    if(D === 0){
+      arr[0] = x;
+    } else if (D > 0){
+      arr[0] = x1;
+      arr[1] = x2;
+    }
   return arr;
 }
 
