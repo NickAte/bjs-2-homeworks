68062fbad272d0ca7e1cc327106f63f55c969d69
NickAte <72125393+NickAte@users.noreply.github.com>
2023-03-02 12:55:31 +0300

    Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
-}
-
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    let res = false;
+    
+        res = arr1.every(function(element, index) {
+            return element === arr2[index]; 
+        });
+    
+        return (arr1.length == arr2.length) && res;
+    }
+    
+    function getUsersNamesInAgeRange(users, gender) {
+      
+    let mas = users.filter(item => item.gender === gender).map(item => item.age);
+    let res = mas.reduce((sum, age) => sum + age, 0);
+    
+    return (mas.length > 0) ? res / mas.length : 0;
+    
+    }
