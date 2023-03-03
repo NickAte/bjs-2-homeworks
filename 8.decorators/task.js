8027f9a180d3e2fd0147263782478e1bd4effa88
NickAte <72125393+NickAte@users.noreply.github.com>
2023-03-03 16:21:48 +0300

    Декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,46 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
+    let cache = [];
+    const maxCacheValuesCount = 5;
+    return (...args) => {
+      const hash = md5(args);
+      const objectFromCache = cache.find(object => object.hash === hash);
+      if (objectFromCache){
+        console.log("Из кэша: ", objectFromCache.value);
+        return "Из кэша: " + objectFromCache.value;
+      }
   
-}
+      const value = func(...args);
+      cache.push({hash, value})
+      if(cache.length > maxCacheValuesCount) {
+        cache.shift();
+      }
+  
+      console.log("Вычисляем: ", value);
+      return "Вычисляем: " + value;
+    };
+  }
+  
 
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
+
+  function debounceDecoratorNew(func, delay) {
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
   
-}
+    function wrapper(...args) {
+      wrapper.allCount++;
+  
+      if(timeoutId === null) {
+        func(...args);
+        wrapper.count++;
+      }
+  
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        wrapper.count++;
+        func(...args);
+      }, delay);
+    }
+  
+    return wrapper;
+  }
